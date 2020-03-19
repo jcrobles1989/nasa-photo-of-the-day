037fc02d9d250b4e51f825c25858806792e2f448
Jose Carlos Robles <jcrobles1989@gmail.com>
2020-03-19 08:53:47 -0700

    copyright done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from 'axios';
+import Copyright from './Components/Copyright';
 
 function App() {
+
+  const [nasaData, setNasaData] = useState([]);
+  
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=KkdVFsCLaJjjSugXW2VhVY4Quzlm4S7D6YffH2LD').then(response => {
+      // console.log(response)
+      setNasaData(response.data)
+    })
+    .catch(error => {
+      console.log('the data was not returned', error);
+    })
+  },[]);
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+      <Copyright copyright={nasaData.copyright}/>
     </div>
   );
 }
---
+++ src/Components/Copyright.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Copyright(props) {
+    return (<p>{props.copyright}</p>)
+}
